e85fbfb5e2fc24354286b7000dbd5237cd7f226a
pjambet <pierre.jambet@gmail.com>
2014-04-07 15:33:13 -0400

    Fix typo : rails v is not a command

--- lib/extra_tasks.rb
+++ lib/extra_tasks.rb
@@ -4,7 +4,7 @@ require 'rake'
 begin
   # If a rake task is ran from the parent applicatiom, all Rails rasks are
   # already loaded.
-  # But if rails {s/v} is ran from the parent application, then tasks are not
+  # But if `rails {s|c}` is ran from the parent application, then tasks are not
   # loaded
   Rake::Task['db:migrate']
 rescue RuntimeError
