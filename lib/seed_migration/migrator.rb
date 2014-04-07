ba6e35c3eb2a09a942a0582b11fc0575296286ef
pjambet <pierre.jambet@gmail.com>
2014-04-07 15:33:33 -0400

    Order models by id (it naively assumes that all models have an id field)

--- lib/seed_migration/migrator.rb
+++ lib/seed_migration/migrator.rb
@@ -199,7 +199,7 @@ module SeedMigration
 ActiveRecord::Base.transaction do
         eos
         SeedMigration.registrar.each do |register_entry|
-          register_entry.model.all.each do |instance|
+          register_entry.model.order('id').each do |instance|
             file.write generate_model_creation_string(instance, register_entry)
           end
 
