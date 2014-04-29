0a86fe383db195f010fbcf5105eff28e4d25addf
pjambet <pierre.jambet@gmail.com>
2014-04-29 17:49:56 +0200

    Rename some remainings 'data_migration' to 'seed_migration'

--- lib/generators/seed_migration/seed_migration_generator.rb
+++ lib/generators/seed_migration/seed_migration_generator.rb
@@ -10,7 +10,7 @@ module SeedMigration
       class_option :migration_name, :type => :string, :default => nil
       argument :timestamp, :type => :string, :required => false, :default => Time.now.utc.strftime("%Y%m%d%H%M%S")
 
-      def create_data_migration_file
+      def create_seed_migration_file
         path = SeedMigration::Migrator::DATA_MIGRATION_DIRECTORY
         create_file path.join("#{timestamp}_#{file_name.gsub(/([A-Z])/, '_\1').downcase}.rb"),"class #{file_name.camelize} < SeedMigration::Migration\n    def up\n        \n    end\n\n    def down\n        \n    end\nend"
       end
