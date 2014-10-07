b0c4b1d03302826a93a69e9f9aceae9a1bd6c49d
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2014-10-07 11:40:25 +0900

    Add ignore_create_seed_file option

--- lib/seed_migration/migrator.rb
+++ lib/seed_migration/migrator.rb
@@ -195,7 +195,7 @@ module SeedMigration
     end
 
     def self.create_seed_file
-      if !Rails.env.development?
+      if !Rails.env.development? || SeedMigration.ignore_create_seed_file
         return
       end
       File.open(SEEDS_FILE_PATH, 'w') do |file|
