5fdf320cb9f389dbf7312a092f42bb35de047bd9
Maher Hawash <gmhawash@gmail.com>
2014-12-17 07:32:15 -0800

    Add Capistrano 3.x tasks

--- lib/capistrano/seed_migration_tasks.rb
+++ lib/capistrano/seed_migration_tasks.rb
@@ -1,18 +1,22 @@
 namespace :seed do
   desc "Run new data migrations."
-  task :migrate => :environment do
+  task :migrate do
     on roles(:all), :only => { :primary => true } do
       within current_path do
-        rake 'seed:migrate'
+        with :rails_env => fetch(:rails_env) do
+          rake 'seed:migrate'
+        end
       end
     end
   end
 
   desc "Revert last data migration."
-  task :rollback => :environment do
+  task :rollback do
     on roles(:all), :only => { :primary => true } do
       within current_path do
-        rake 'seed:rollback'
+        with :rails_env => fetch(:rails_env) do
+          rake 'seed:rollback'
+        end
       end
     end
   end
