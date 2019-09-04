632e0bfd2c720fa9ce9ed1d8f7f4ec0c345a559f
Brian Freese <brianfreese@hotmail.com>
2019-09-04 12:39:34 -0500

    Adds support for Rails 6.  Lazy-load register model configuration to avoid autoloader depreciations.  Also should fix rails 5.2 schema load issues when in test environment.

--- lib/seed_migration.rb
+++ lib/seed_migration.rb
@@ -20,7 +20,7 @@ module SeedMigration
     end
 
     def unregister(model)
-      self.registrar.delete_if { |entry| entry.model == model }
+      self.registrar.delete_if { |entry| entry.model_name == model.to_s }
     end
   end
 end
